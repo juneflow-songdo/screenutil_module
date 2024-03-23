3c4082f397dd566a44b68f7a649d4e85135aa0d5
june lee <melodysdreamj@gmail.com>
2024-03-23 22:29:44 +0900

    Initial commit

---
+++ lib/util/_/initial_app/build_app_widget/build_my_app/screenutil_module/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverScreenutilModule(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
---
+++ lib/util/_/initial_app/ready_functions/before_material_app/screenutil_module/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForScreenutilModule(BuildContext context) async {}
